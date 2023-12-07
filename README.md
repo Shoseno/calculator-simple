91cccbba24e9994646594d2c3031427a91db2d2c
Shoseno <129028927+Shoseno@users.noreply.github.com>
2023-12-06 19:10:51 -0500

    Add website layout and button functionality

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # calculator-simple
 web calculator that can handle basic operations. 
+
+current bugs: doing more that two operations at once breaks the machine. if you input 15 + 15 then + again. it will show to answer but not clear it upon new inputs. 
--- scripts.js
+++ scripts.js
@@ -19,15 +19,86 @@ function divide(x, y) {
 let num1;
 let num2;
 let operator;
+let answer;
 
 function operate(operator, num1, num2) {
     if (operator === 'add') {
-       return add(num1, num2);
+       answer =  add(num1, num2);
     } else if (operator === 'subtract') {
-        return subtract(num1, num2);
+        answer = subtract(num1, num2);
     } else if (operator === 'multiply') {
-        return multiply(num1, num2);
+        answer =  multiply(num1, num2);
     } else if (operator === 'divide') {
-        return divide(num1, num2);
+        answer = divide(num1, num2);
     }
+}
+
+const content = document.querySelector('.content');
+let lastClickedBtnId = '';
+let lastClickedBtn = '';
+let lastClickedBtnInner = '';
+
+//adds event listener to all buttons and shows last pressed
+content.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    }
+    lastClickedBtnId = (event.target.id);
+    lastClickedBtn = (event.target);
+    lastClickedBtnInner = (event.target.innerHTML);
+} );
+
+let display = document.querySelector('.display');
+function clearDisplay() {
+    display.innerHTML = '';  
+}
+
+const digits = document.querySelectorAll('.digit');
+digits.forEach(function(digitBtn) {
+    digitBtn.addEventListener('click', function() {
+        display.innerHTML += digitBtn.innerHTML;
+    })
+});
+
+
+const operators = document.querySelector('.operator-pad');
+
+operators.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    } else if (operator === 'add' 
+            || operator === 'subtract' 
+            || operator === 'multiply' 
+            || operator === 'divide') {
+                calculate();
+                return;
+            }
+    num1 = +display.innerHTML;
+    operator = event.target.id;
+    clearDisplay();
+});
+
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', function calculate() {
+    num2 = +display.innerHTML;
+    console.log(num1);
+    console.log(operator);
+    console.log(num2);
+    clearDisplay();
+    operate(operator, num1, num2);
+    display.innerHTML = answer;
+    operator = '';
+});
+
+function calculate() {
+    num2 = +display.innerHTML;
+    console.log(num1);
+    console.log(operator);
+    console.log(num2);
+    clearDisplay();
+    operate(operator, num1, num2);
+    display.innerHTML = answer;
+    operator = '';
 }
